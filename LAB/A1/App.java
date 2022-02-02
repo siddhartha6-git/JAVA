e9e28874069448cfe3d3a62efa9c4f9fdfbf5b1c
siddhartha6-git <91537778+siddhartha6-git@users.noreply.github.com>
2022-02-02 17:08:35 +0530

    Update App.java

--- LAB/A1/App.java
+++ LAB/A1/App.java
@@ -4,72 +4,29 @@
 @since: 02-Feb-2022
 */
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.io.Console;
+public class App {
+    public static void main(String[] args) {
+        Student student_1 = new Student();
+        Console cons_input = System.console();
+        char[] password;
+        String username;
 
-public class Student {
-    //################### attributes ##########################
-    static int course_credits = 3;
-    ArrayList<Integer> marks = new ArrayList<Integer> (Arrays.asList(55, 66, 77, 88, 99, 100));
+        cons_input.printf("Enter Username: ");
+        username = cons_input.readLine();
+        cons_input.printf("Enter Password: ");
+        password =  cons_input.readPassword();
+        while (!student_1.validateLogin(username, password)) { //validating user
+            cons_input.printf("Invalid credentials...\n");
+            cons_input.printf("Enter Username: ");
+            username = cons_input.readLine();
+            cons_input.printf("Enter Password: ");
+            password =  cons_input.readPassword();
+        }     
+        cons_input.printf("Welcome %s...\n", username);
 
-    public Student() {
-    }
-
-    //################### validateLogin ##########################
-    public boolean validateLogin(String username, char[] password) {
-        if (username.equals(String.valueOf(password))) {
-            return false;
-        }
-        else {
-            return true;
-        }
-    }
-
-    //################### get_marks ##########################
-    public ArrayList<Integer> get_marks() {
-       return this.marks;
-    }
-
-    //################### calculate_grade ##########################
-    public ArrayList<Character> calculate_grade() {
-        ArrayList<Character> grades = new ArrayList<Character>();
-        for (int mark : this.marks) {
-            if (mark >= 90) {
-                grades.add('S');
-            }
-            else if (mark >= 80) {
-                grades.add('A');
-            }
-            else if (mark >= 70) {
-                grades.add('B');
-            }
-            else if (mark >= 60) {
-                grades.add('C');
-            }
-            else if (mark >= 55) {
-                grades.add('D');
-            }
-            else if (mark >= 50) {
-                grades.add('E');
-            }
-            else {
-                grades.add('F');
-            }
-        }
-        return grades;
-    }
-
-    //################### get_gpa ##########################
-    public double get_gpa() {
-        double total_marks = 0;
-        for (int mark : marks) {
-            if (mark >= 50 && mark < 60) {
-                total_marks += (mark/10);               
-            }
-            else {
-                total_marks += (mark/10) + 1;
-            }
-        }
-        return total_marks/(this.marks.size());
+        System.out.println(student_1.get_marks());
+        System.out.println(student_1.calculate_grade());
+        System.out.println(student_1.get_gpa());
     }
 }
